7b48f9036a0e80e12043f5457bcac4a255b7720b
rickbeeloo <r.beeloo@outlook.com>
2025-04-03 17:14:39 +0200

    remove timing print

--- pa-bitpacking/src/profile.rs
+++ pa-bitpacking/src/profile.rs
@@ -1,7 +1,6 @@
 use bio::alphabets::{Alphabet, RankTransform};
 use itertools::Itertools;
 use pa_types::{Seq, I};
-use std::time::Instant;
 use crate::{B, W};
 
 /// Builds a 'profile' of `b` in `64`-bit blocks, and compressed `a` into a `[0,1,2,3]` alphabet.
@@ -48,13 +47,12 @@ impl Profile for ScatterProfile {
                 x => panic!("Unknown base {}", x as char),
             }
         }
-        let start_time = Instant::now();
         let pa = a.iter().map(|ca| CC(get_char(*ca))).collect_vec();
         let mut pb = vec![[0; 4]; b.len().div_ceil(W)];
         for (j, cb) in b.iter().enumerate() {
             let mask = get_mask(*cb);
-            for i in 0..4 {
-                pb[j / W][i] |= mask[i] << (j % W);
+            for (i, &m) in mask.iter().enumerate() {
+                pb[j / W][i] |= m << (j % W);
             }
         }
         for j in b.len()..b.len().next_multiple_of(W) {
@@ -62,7 +60,6 @@ impl Profile for ScatterProfile {
                 *x |= 1 << (j % W);
             }
         }
-        println!("Time taken: {:?}", start_time.elapsed());
         (pa, pb)
     }
 
